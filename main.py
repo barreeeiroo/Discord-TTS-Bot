79856771142332aebc4823e348d63cf65c7cf5dc
Diego Barreiro <diego.barreiro.perez@gmail.com>
2021-05-29 17:56:54 +0200

    Fixes

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import asyncio
 import os
+import re
 
 import discord
 from discord.ext import commands
 from discord.utils import get
 from gtts import gTTS
 
-PREFIX = '!'
+PREFIX = '$'
 bot = commands.Bot(command_prefix=PREFIX)
+
+
+def cleanemojis(string):
+    return re.sub(r"<a?:([a-zA-Z0-9_-]{1,32}):[0-9]{17,21}>", r":\1:", string)
 
 
 def get_voiceclient(message):
     voice_client = get(bot.voice_clients, guild=message.guild)
     return voice_client
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Mudi Inteando'))
+    print('Connected to bot: {}'.format(bot.user.name))
+    print('Bot ID: {}'.format(bot.user.id))
 
 
 @bot.event
@@ -45,13 +57,17 @@ async def on_message(message):
         # Only play if in the same voice channel
         return
 
+    if "tts" not in message.channel.name.lower():
+        # Only accept messages in tts channel
+        return
+
     while vc.is_playing():
         await asyncio.sleep(0.1)
 
-    tts = gTTS(user.nick + ' dice, ' + message.clean_content, lang='es', tld='es')
+    tts = gTTS(user.nick + ' dice, ' + cleanemojis(message.clean_content), lang='es', tld='es')
     tts.save("msg.mp3")
 
-    vc.play(discord.FFmpegPCMAudio(source='msg.mp3', executable='C:\\ffmpeg\\ffmpeg.exe'))
+    vc.play(discord.FFmpegPCMAudio(source='msg.mp3', executable='C:\\ffmpeg\\ffmpeg.exe', options = "-loglevel panic"))
 
 
 @bot.command(name="ven")
