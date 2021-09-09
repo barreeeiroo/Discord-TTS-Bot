d93ae65a78cfc39164538ea94cce0ae47f6d6196
Diego Barreiro <diego.barreiro.perez@gmail.com>
2021-09-09 22:44:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def get_voiceclient(message):
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Mudi Inteando'))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Barreiro smurfing con Leona'))
     print('Connected to bot: {}'.format(bot.user.name))
     print('Bot ID: {}'.format(bot.user.id))
 
