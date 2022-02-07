5e8c7ef355a65f9d17dd388577a3cdcfe48b146e
adrian200223 <63094944+adrian200223@users.noreply.github.com>
2022-02-07 18:15:49 +0100

    added auto disconnect

--- main.py
+++ main.py
@@ -294,6 +294,12 @@ async def on_voice_state_update(member, before, after):
             texto = 'Chao ' + (member.nick if member.nick is not None else member.name)
         else:
             texto = 'Hola ' + (member.nick if member.nick is not None else member.name)
+    
+    if texto.startswith('Chao') and len(vc.channel.members) == 0:
+        if not vc or not vc.is_connected():
+            # If no voice channel, ignore
+            return
+        await vc.disconnect()
 
     while vc.is_playing():
         await asyncio.sleep(0.1)
